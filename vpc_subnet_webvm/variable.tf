c6be7db72c065a763ca1d19fe78cb427d284cbce
rtattaluri <rt.attaluri@gmail.com>
2023-08-31 00:44:32 +1000

    add terraform files

---
+++ vpc_subnet_webvm/variable.tf
@@ -0,0 +1,27 @@
+variable "aws_region"{
+    type = string
+}
+variable "aws_access_key"{
+    type = string
+}
+variable "aws_secret_key"{
+    type = string
+}
+variable "vpc_name"{
+    type = string
+}
+variable "vpc_cidr"{
+    type = string
+}
+variable "subnet_cidr_range"{
+    type = string
+}
+variable "ami_image"{
+    type = string
+}
+variable "ec2_size"{
+    type = string
+}
+variable "private_key_pair_name"{
+    type = string
+}
